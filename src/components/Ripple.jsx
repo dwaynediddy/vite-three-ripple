3e7014ed6545d8adaaf4964b4f62b3e2574ea109
dwaynediddy <burrows.dwayne@icloud.com>
2021-05-18 16:03:21 +0200

    added ripple

--- src/components/Ripple.jsx
+++ src/components/Ripple.jsx
@@ -1,10 +1,17 @@
-import React, { Suspense, useMemo } from 'react'
+import React, { Suspense, useMemo, useCallback } from 'react'
 import * as THREE from 'three'
 import { Canvas, useLoader } from 'react-three-fiber'
 import circle from './assets/circle.png'
 
 function Points() {
   const imgTex = useLoader(THREE.TextureLoader, circle)
+
+  let t = 0
+  let f = 0.002
+  let a = 3
+  const graph = useCallback((x, z) => {
+      return Math.sin(f * (x ** 2 + z ** 2 + t)) * a
+  }, [t, f, a])
 
   const count = 100
   const sep = 3
@@ -15,13 +22,13 @@ function Points() {
           for(let zi = 0; zi < count; zi++){
               let x = sep * (xi - count / 2)
               let z = sep * (zi - count / 2)
-              let y = 0
+              let y = graph(x, z)
               positions.push(x, y, z)
           }
       }
 
       return new Float32Array(positions)
-  }, [count, sep])
+  }, [count, sep, graph])
 
   return(
       <points>
